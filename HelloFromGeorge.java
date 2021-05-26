eaab5b44be873ad49809dafaf89c7dd153813109
George Van Thiel Berghuijs <83961366+georgewvtb@users.noreply.github.com>
2021-05-26 11:23:17 +0200

    practicing

--- HelloFromGeorge.java
+++ HelloFromGeorge.java
@@ -3,7 +3,6 @@ public class HelloFromGeorge {
         public static void main(String[] args) {
 
                 System.out.println("Hello, my name is George.");
-                System.out.println("Stretch challenge!");
         }
 
 }
